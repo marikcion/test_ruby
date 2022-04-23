912838468f8d62b9b4cedbfb9835f6f0cac8b504
marikcion <marikcion@gmail.com>
2022-04-23 15:33:24 +0300

    requsts

---
+++ class_get.rb
@@ -0,0 +1,11 @@
+require 'curb'
+
+class Zapros
+
+    def get_requsts(url)
+        html = Curl::Easy.http_get(url) do |html|
+            html.headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36'
+            html.headers['Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+        end
+    end
+end
